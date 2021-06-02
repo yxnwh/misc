2ef0a4c340ce1e4e5e699884300f9677ea26d52f
yxnwh <36069159+yxnwh@users.noreply.github.com>
2021-06-02 11:17:09 +0800

    Update jd_priceProtect.js

--- jd/jd_priceProtect.js
+++ jd/jd_priceProtect.js
@@ -7,7 +7,7 @@ update 2021/4/11
 ==========================Quantumultx=========================
 [task_local]
 # 京东价格保护
-5 0 * * * https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/jd_priceProtect.js, tag=京东价格保护, img-url=https://raw.githubusercontent.com/ZCY01/img/master/pricev1.png, enabled=true
+5 0 * * * https://raw.githubusercontent.com/yxnwh/misc/main/jd/jd_priceProtect.js, tag=京东价格保护, img-url=https://raw.githubusercontent.com/ZCY01/img/master/pricev1.png, enabled=true
 */
 const $ = new Env('京东价格保护');
 
