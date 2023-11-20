3822e4235fe0d03bf4e6ecc91cdafdb2f35a366c
mark elvens <36069159+yxnwh@users.noreply.github.com>
2023-11-20 16:04:29 +0800

    Create JRSYXJ.js

---
+++ JRSYXJ.js
@@ -0,0 +1,9 @@
+var objc = JSON.parse($response.body);
+
+    objc = 
+{
+  "result": "bncdzrxa+Dq49i7NxArtSeZtLpZ7cxsOynuCD5Co47mgXj5V8wuwHKrBZk6XMBm14RbGudMdnG9/bN5HYEawsYzr4IJtgtQeWEXeLX+5qSlN9uDunZC4Il0PpaxdeEaYm4TemFHyNAcFl6A+0LgQv/jmaZGldDdypZEYBC8Xm45u8HSvDEPsIoPk8CXEWpXBLJvSfsZ+3ryV87nwgLjZGjR301rTrcvAmuC8gr3p3tU2KOawe5xnH9sNYfeg2Vk1wwtltF6LObXU+CNxqqDTj4EKvbOxGrqB4R/ZZ8wNs/94joVG1o3cf5hydFNgulEN"
+}
+
+
+$done({body : JSON.stringify(objc)});
